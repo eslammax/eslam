fbf685a2227b6cc25fa87fda8d5944b4e6a5065b
eslammax <eslammx@gmail.com>
2016-11-12 19:14:49 +0200

    Update and rename README.md to eslam.js

---
+++ eslam.js
@@ -0,0 +1,621 @@
+
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+
+<script type="text/javascript">
+
+</script>
+
+
+<link href="http://cdn.kooralive.info/assets/css/styles.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<script src="http://cdn.kooralive.info/assets/js/bootstrap.min.js"></script>
+<script src="http://cdn.kooralive.info/assets/js/jquery.spasticNav.js"></script>
+<script src="http://cdn.kooralive.info/assets/js/jquery.scrollUp.min.js"></script>
+
+
+
+
+<script src="http://cdn.kooralive.info/assets/js/scripts.js?3"></script>
+<script type="text/javascript">
+
+</script>
+
+<body>
+
+
+<div class="container">
+<div id="top" class="row">
+<div class="col-sm-4">
+<span class="date"></span>
+</div>
+<div class="col-sm-4">
+
+</div>
+<div class="col-sm-4">
+<div class="social">
+
+</div>
+</div>
+</div>
+<div>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+<span class="sr-only"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+
+</div>
+<div class="collapse navbar-collapse">
+<ul class="nav navbar-nav">
+
+
+<div id="content">
+<div class="container">
+<div class="row">
+<div class="top carousel slide" data-ride="carousel">
+<div class="carousel-inner" role="listbox">
+<div class="item active">
+<div class="col-md-3 col-sm-6">
+
+
+
+<div class="row">
+<div class="col-md-4 col-sm-6">
+
+
+<b></b>
+</div>
+
+
+<div></div>
+
+</div>
+</div>
+<div class="col-md-8 col-sm-6">
+<div class="ad">
+<a href="">
+<img src=""/>
+</a>
+</div>
+<div class="matches">
+<div class="header">
+<a data-id="-1" href="#">مباريات الامس</a>
+<a data-id="0" class="active" href="#">مباريات اليوم</a>
+<a data-id="1" href="#">مباريات الغد</a>
+</div>
+<div id="matches--1" class="rows">
+<div>
+<div class="open">
+<a class="bordered-button" href="http://www.mol5sat.net/2016/11/Brazil-Vs-Argentina.html" target="_blank">شاهد الاهداف والملخصات</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+انتهت المباراة
+</div>
+<div class="data">
+<div class="team">
+<img alt="البرازيل" src="http://cdn.kooralive.info/uploads/1478707188-البرازيل.png"/>
+<span>البرازيل</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">3</span>
+<span class="vs">VS</span>
+<span class="score">0</span>
+</div>
+<a href="http://www.mol5sat.net/2016/11/Brazil-Vs-Argentina.html" class="red-button">شاهد الاهداف والملخصات</a>
+</div>
+<div class="team">
+<img alt="الأرجنتين" src="http://cdn.kooralive.info/uploads/1478707188-الأرجنتين.png"/>
+<span>الأرجنتين</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> رؤوف بن خليف </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> BeIN Sports 1 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://www.mol5sat.net/2016/11/Libya-Vs-Tunisia.html" target="_blank">شاهد الاهداف والملخصات</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+انتهت المباراة
+</div>
+<div class="data">
+<div class="team">
+<img alt="ليبيا" src="http://cdn.kooralive.info/uploads/1478790910-ليبيا.png"/>
+<span>ليبيا</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">0</span>
+<span class="vs">VS</span>
+<span class="score">1</span>
+</div>
+<a href="http://www.mol5sat.net/2016/11/Libya-Vs-Tunisia.html" class="red-button">شاهد الاهداف والملخصات</a>
+</div>
+<div class="team">
+<img alt="تونس" src="http://cdn.kooralive.info/uploads/1478790910-تونس.png"/>
+<span>تونس</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> حفيظ دراجي </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> BeIN Sports 2 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://www.mol5sat.net/2016/11/San-Marino-Vs-Germany.html" target="_blank"> شاهد الاهداف | Watch Match Goals</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+انتهت المباراة
+</div>
+<div class="data">
+<div class="team">
+<img alt="ألمانيا" src="http://cdn.kooralive.info/uploads/1478786817-ألمانيا.png"/>
+<span>ألمانيا</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">8</span>
+<span class="vs">VS</span>
+<span class="score">0</span>
+</div>
+<a href="http://www.mol5sat.net/2016/11/San-Marino-Vs-Germany.html" class="red-button">شاهد الاهداف</a>
+</div>
+<div class="team">
+<img alt="سان مارينو	" src="http://cdn.kooralive.info/uploads/1478786817-سان-مارينو.png"/>
+<span>سان مارينو </span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> أحمد الحامد </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+5 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كأس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://www.mol5sat.net/2016/11/England-Vs-Scotland.html" target="_blank">شاهد الاهداف والملخصات</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+انتهت المباراة
+</div>
+<div class="data">
+<div class="team">
+<img alt="إنجلترا" src="http://cdn.kooralive.info/uploads/1478788689-إنجلترا.png"/>
+<span>إنجلترا</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">3</span>
+<span class="vs">VS</span>
+<span class="score">0</span>
+</div>
+<a href="http://www.mol5sat.net/2016/11/England-Vs-Scotland.html" class="red-button">شاهد الاهداف والملخصات</a>
+</div>
+<div class="team">
+<img alt="إسكوتلندا" src="http://cdn.kooralive.info/uploads/1478788689-إسكوتلندا.png"/>
+<span>إسكوتلندا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> علي حميد </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+4 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كأس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://www.mol5sat.net/2016/11/France-Vs-Sweden.html" target="_blank">شاهد الاهداف والملخصات</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+انتهت المباراة
+</div>
+<div class="data">
+<div class="team">
+<img alt="فرنسا" src="http://cdn.kooralive.info/uploads/1478789737-فرنسا.png"/>
+<span>فرنسا</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">2</span>
+<span class="vs">VS</span>
+<span class="score">1</span>
+</div>
+<a href="http://www.mol5sat.net/2016/11/France-Vs-Sweden.html" class="red-button">شاهد الاهداف والملخصات</a>
+</div>
+<div class="team">
+<img alt="السويد" src="http://cdn.kooralive.info/uploads/1478789737-السويد.png"/>
+<span>السويد</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> علي سعيد الكعبي </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+3 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كأس العالم </div>
+</div>
+</div>
+</div>
+</div>
+<div id="matches-0" class="rows active">
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12320-Algeria-vs-Nigeria.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="نيجيريا" src="http://cdn.kooralive.info/uploads/1478876932-نيجيريا.png"/>
+<span>نيجيريا</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-12T16:00:00Z"></div>
+</div>
+<div class="team">
+<img alt="الجزائر" src="http://cdn.kooralive.info/uploads/1478876932-الجزائر.png"/>
+<span>الجزائر</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> حفيظ دراجي </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> BeIN Sports 1 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12317-Wales-vs-Serbia.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="ويلز" src="http://cdn.kooralive.info/uploads/1478871827-ويلز.png"/>
+<span>ويلز</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-12T19:45:00Z"></div>
+</div>
+<div class="team">
+<img alt="صربيا" src="http://cdn.kooralive.info/uploads/1478871827-صربيا.png"/>
+<span>صربيا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> أحمد الحامد </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+5 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12318-Italy-vs-Liechtenstein.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="ليشتنشتاين" src="http://cdn.kooralive.info/uploads/1478875534-ليشتنشتاين.png"/>
+<span>ليشتنشتاين</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-12T19:45:00Z"></div>
+</div>
+<div class="team">
+<img alt="إيطاليا" src="http://cdn.kooralive.info/uploads/1478875534-إيطاليا.png"/>
+<span>إيطاليا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> عصام عبدة </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+4 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12319-Spain-vs-Macedonia.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="أسبانيا" src="http://cdn.kooralive.info/uploads/1478876281-أسبانيا.png"/>
+<span>أسبانيا</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-12T19:45:00Z"></div>
+</div>
+<div class="team">
+<img alt="مقدونيا" src="http://cdn.kooralive.info/uploads/1478876281-مقدونيا.png"/>
+<span>مقدونيا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> محمد الشامسي </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+3 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12321-Morocco-vs-ivory-coast.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="المغرب" src="http://cdn.kooralive.info/uploads/1478877688-المغرب.png"/>
+<span>المغرب</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-12T20:00:00Z"></div>
+</div>
+<div class="team">
+<img alt="ساحل العاج	" src="http://cdn.kooralive.info/uploads/1478877688-ساحل-العاج.png"/>
+<span>ساحل العاج </span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> غير معروف </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> maghrabi </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+</div>
+<div id="matches-1" class="rows">
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12324-Egypt-vs-Ghana.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="مصر" src="http://cdn.kooralive.info/uploads/1478957825-مصر.png"/>
+<span>مصر</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-13T16:00:00Z"></div>
+</div>
+<div class="team">
+<img alt="غانا" src="http://cdn.kooralive.info/uploads/1478957825-غانا.png"/>
+<span>غانا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> مدحت شلبي </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> On sport </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12322-Netherlands-vs-Luxembourg.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="لوكسمبرج" src="http://cdn.kooralive.info/uploads/1478956607-لوكسمبرج.png"/>
+<span>لوكسمبرج</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-13T17:00:00Z"></div>
+</div>
+<div class="team">
+<img alt="هولندا" src="http://cdn.kooralive.info/uploads/1478956607-هولندا.png"/>
+<span>هولندا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> أحمد الحامد </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+3 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+<div>
+<div class="open">
+<a class="bordered-button" href="http://kooralive.info/12323-Portugal-vs-Latvia.html">شاهد المباراة | Watch the match</a>
+</div>
+<div class="match-row">
+<div class="match">
+<div class="info">
+<span class="live" style="display: none">جارية الان</span>
+</div>
+<div class="data">
+<div class="team">
+<img alt="البرتغال" src="http://cdn.kooralive.info/uploads/1478957137-البرتغال.png"/>
+<span>البرتغال</span>
+</div>
+<div class="center">
+<div class="result">
+<span class="score">&nbsp;</span>
+<span class="vs">VS</span>
+<span class="score">&nbsp;</span>
+</div>
+<div class="time" data-time="2016-11-13T19:45:00Z"></div>
+</div>
+<div class="team">
+<img alt="لاتفيا" src="http://cdn.kooralive.info/uploads/1478957137-لاتفيا.jpg"/>
+<span>لاتفيا</span>
+</div>
+</div>
+</div>
+<div class="meta">
+<div class="mic">
+<img src="http://cdn.kooralive.info/assets/images/mic.png"/> لي سعيد الكعبي </div>
+<div class="tv hidden-xs hidden-sm">
+<img src="http://cdn.kooralive.info/assets/images/tv.png"/> Abu D+4 </div>
+<div class="cup">
+<img src="http://cdn.kooralive.info/assets/images/cup.png"/> تصفيات كاس العالم </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-66230248-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+<div id="footer">
+</a>
+</div>
+</div>
+</body>
+</html>
