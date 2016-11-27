c06d376155a6b6050b2224aea20ba1d8e0daa516
eslammax <eslammx@gmail.com>
2016-11-28 00:59:03 +0200

    Create aa.js

---
+++ aa.js
@@ -0,0 +1,131 @@
+<script>
+function setURL(url){
+    document.getElementById('iframe').src = url;
+}
+</script>
+<link type='text/css' rel='stylesheet' href='https://www.blogger.com/static/v1/widgets/3945106653-widget_css_bundle_rtl.css' />
+<link type='text/css' rel='stylesheet' href='https://www.blogger.com/dyn-css/authorization.css?targetBlogID=2137306810622292363&amp;zx=12055637-9ae9-4f26-92ca-b3aa2ea67bd9' />
+<style id='page-skin-1' type='text/css'><!--
+/*
+
+--></style>
+<style type='text/css'>
+
+body {width: 100%;
+font-family: "DroidKufi-regular" ;
+background-color:#000;
+text-align:center;
+float: center ;
+margin: 0px 0px 0px 0px ;
+}
+
+ 
+@font-face {
+  font-family: 'DroidKufi-regular';
+  font-style: normal;
+  font-weight: 400;
+  src: url(//themes.googleusercontent.com/static/fonts/earlyaccess/droidarabickufi/v3/DroidKufi-Regular.eot);
+  src: url(//themes.googleusercontent.com/static/fonts/earlyaccess/droidarabickufi/v3/DroidKufi-Regular.eot?#iefix) format('embedded-opentype'),
+       url(//themes.googleusercontent.com/static/fonts/earlyaccess/droidarabickufi/v3/DroidKufi-Regular.woff2) format('x-woff2'),
+       url(//themes.googleusercontent.com/static/fonts/earlyaccess/droidarabickufi/v3/DroidKufi-Regular.woff) format('woff'),
+       url(//themes.googleusercontent.com/static/fonts/earlyaccess/droidarabickufi/v3/DroidKufi-Regular.ttf) format('truetype');
+}
+
+
+.button, .button:visited {
+ background-color: #ECECEC ;
+ display: inline-block;
+ color: #000;
+ text-decoration: none;
+ -moz-border-radius: 4px;
+ -webkit-border-radius: 4px;
+ position: relative;
+ cursor: pointer;
+ font-family: 'DroidKufi-regular', Tahoma;
+border: 0px;
+margin-top: 4px;
+}
+
+.button:hover {
+ background-color: #F9F9F9;
+ color: #000;
+}
+
+.button:active {
+ top: 1px;
+}
+
+.medium.button, .large.button:visited {
+ font-size: 15px;
+ padding: 1px 4px 1px;
+}
+
+.pink.button, .magenta.button:visited {
+ background-color: #e22092;
+}
+
+.pink.button:hover {
+ background-color: #c81e82;
+}
+
+.green.button, .green.button:visited {
+ background-color: #91bd09;
+}
+
+.green.button:hover {
+ background-color: #749a02;
+}
+
+.red.button, .red.button:visited {
+ background-color: #e62727;
+}
+
+.red.button:hover {
+ background-color: #cf2525;
+}
+
+.orange.button, .orange.button:visited {
+ background-color: #ff5c00;
+}
+
+.orange.button:hover {
+ background-color: #d45500;
+}
+
+.blue.button, .blue.button:visited {
+ background-color: #2981e4;
+}
+
+.blue.button:hover {
+ background-color: #2575cf;
+}
+
+.yellow.button, .yellow.button:visited {
+ background-color: #ffb515;
+}
+
+.yellow.button:hover {
+ background-color: #fc9200;
+}
+
+
+</style>
+</head>
+<body>
+<div class='head section' id='head'><div class='widget HTML' data-version='1' id='HTML1'>
+<div class='widget-content'>
+<a onclick="window.open('#fullscreen')" class="medium button yellow" >&#1588;&#1575;&#1588;&#1607; &#1603;&#1575;&#1605;&#1604;&#1607;</a>
+
+<a onclick="javascript:location.reload()" class="medium button red" >&#1578;&#1581;&#1583;&#1610;&#1579; &#1575;&#1604;&#1602;&#1606;&#1575;&#1577;</a>
+<a onclick="window.open('http://footballegy1.blogspot.com/')" class="medium button green" >&#1605;&#1576;&#1575;&#1585;&#1610;&#1575;&#1578; &#1575;&#1604;&#1610;&#1608;&#1605;</a>
+</div>
+
+
+<div class='post-body' id='post-body-62753711917529370' itemprop='description articleBody'>
+<button class="button" onclick="setURL('http://kora-live.tv/live/bein1HD.html')">جوده عاليه</button>
+<button class="button" onclick="setURL('http://kora-live.tv/live/bein1low.html')">جوده متوسطه</button>
+<button class="button" onclick="setURL('http://kora-live.tv/live/bein1sd.html')">جوده ضعيفه</button>
+
+<iframe  id="iframe" align="middle" frameborder="0" height="85%" scrolling="NO" src="http://kora-live.tv/live/bein1.html" width="100%"></iframe>
+<div style='clear: both;'></div>
+</div>
